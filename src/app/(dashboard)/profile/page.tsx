ff31545884c9ad68afc079b31bc1ba32ba7a5410
Mohsen Ashrafi <mohsenashrafi238@gmail.com>
2025-07-26 18:35:38 +0430

    new Updated page

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,10 +1,14 @@
+export const dynamic = "force-dynamic";
+
 import { Suspense } from "react";
+
 import CardsWrapper from "./_components/CardsWrapper";
 import Fallback from "@/ui/Fallback";
 import LatestPosts from "./_components/LatestPosts";
 
 async function Profile() {
   return (
+
     <div>
       <h1 className="text-xl mb-8 text-secondary-700">Dashboard</h1>
       <Suspense fallback={<Fallback />}>
