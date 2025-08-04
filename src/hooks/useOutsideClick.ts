2cf3b3252df98a60a1c8078e10e0423159dc9465
Mohsen Ashrafi <mohsenashrafi238@gmail.com>
2025-08-05 00:32:20 +0430

    document updated

--- src/hooks/useOutsideClick.ts
+++ src/hooks/useOutsideClick.ts
@@ -1,23 +1,50 @@
+// import { RefObject, useEffect, useRef } from "react";
+
+// export default function useOutsideClick(
+//     handler: () => void,
+//     listenCapturing = true
+// ): RefObject<HTMLDivElement> {
+//     const ref = useRef<HTMLDivElement>(null)
+
+//     useEffect(() => {
+//         function handleClick(e: MouseEvent) {
+//             if (ref.current && !ref.current.contains(e.target as Node)) {
+//                 handler()
+//             }
+//         }
+
+//         document.addEventListener("click", handleClick, listenCapturing)
+
+//         return () =>
+//             document.removeEventListener("click", handleClick, listenCapturing)
+//     }, [handler, listenCapturing])
+
+//     return ref
+// }
+
+"use client";
+
 import { RefObject, useEffect, useRef } from "react";
 
 export default function useOutsideClick(
     handler: () => void,
     listenCapturing = true
 ): RefObject<HTMLDivElement> {
-    const ref = useRef<HTMLDivElement>(null)
+    const ref = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
+        if (typeof document === "undefined") return;
+
         function handleClick(e: MouseEvent) {
             if (ref.current && !ref.current.contains(e.target as Node)) {
-                handler()
+                handler();
             }
         }
 
-        document.addEventListener("click", handleClick, listenCapturing)
-
+        document.addEventListener("click", handleClick, listenCapturing);
         return () =>
-            document.removeEventListener("click", handleClick, listenCapturing)
-    }, [handler, listenCapturing])
+            document.removeEventListener("click", handleClick, listenCapturing);
+    }, [handler, listenCapturing]);
 
-    return ref
+    return ref;
 }
