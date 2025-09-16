6e78233d54b35157e95d0f641710faf663bebe57
Mohsen Ashrafi <mohsenashrafi238@gmail.com>
2025-09-16 22:26:31 +0200

    Update page.tsx

--- src/app/(blogs)/blogs/[postSlug]/page.tsx
+++ src/app/(blogs)/blogs/[postSlug]/page.tsx
@@ -1,7 +1,7 @@
 import { getPostSlug, getPosts } from "@/services/postServices";
 import Image from "next/image";
 import { JSX } from "react";
-import { Post} from "types/ApiTypes";
+import { Post } from "types/ApiTypes";
 import RelatedPosts from "../_components/RelatedPosts";
 import PostComment from "../_components/comment/PostComment";
 import NotFound from "./not-found";
@@ -10,20 +10,12 @@ interface SinglePostProps {
   params: Promise<{ postSlug: string }>;
 }
 
-// export async function generateMetadata({ params }: SinglePostProps) {
-//   const post = await getPostSlug(params.postSlug);
-
-//   return {
-//     title: `${post.title}`,
-//   };
-// }
-
 export async function generateMetadata({ params }: SinglePostProps) {
   const { postSlug } = await params;
   const post: Post = await getPostSlug(postSlug);
 
   return {
-    title: post ? `${post.title}` : "Post not found",
+    title: post ? post.title : "Post not found",
   };
 }
 
@@ -34,30 +26,31 @@ export async function generateStaticParams() {
 }
 
 async function SinglePost({ params }: SinglePostProps): Promise<JSX.Element> {
-  // const post = await getPostSlug(params.postSlug);
   const { postSlug } = await params;
   const post: Post = await getPostSlug(postSlug);
-  console.log(post)
+
   if (!post) return NotFound();
+
+  const isExternalImage =
+    post.coverImage.startsWith("http://") || post.coverImage.startsWith("https://");
 
   return (
     <div className="text-secondary-600 max-w-screen-md mx-auto">
-      <h1 className="text-secondary-700 text-2xl font-bold mb-8">
-        {post.title}
-      </h1>
+      <h1 className="text-secondary-700 text-2xl font-bold mb-8">{post.title}</h1>
       <p className="mb-8">{post.text}</p>
-      <div className="relative aspect-video overflow-hidden rounded-lg mb-10">
+
+      <div className="relative w-full h-[450px] overflow-hidden rounded-lg mb-10">
         <Image
           className="object-cover object-center hover:scale-110 transition-all ease-out duration-300"
+          src={post.coverImage}
+          alt={post.briefText || post.title}
           fill
-          src={post?.coverImage}
-          alt={post.briefText}
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 800px"
+          unoptimized={!isExternalImage} 
         />
       </div>
-      {/* {post.related.length > 0 && <RelatedPosts posts={post.related} />} */}
-      {post.related && post.related.length > 0 && (
-        <RelatedPosts posts={post.related} />
-      )}
+
+      {post.related && post.related.length > 0 && <RelatedPosts posts={post.related} />}
       <PostComment post={post} />
     </div>
   );
